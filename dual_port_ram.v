9b2f9e59504d6f755da469d98d76a932a606802e
Srivizna Pedagandham <160113841+vizna1@users.noreply.github.com>
2025-03-18 16:06:18 +0530

    Update dual_port_ram.v

--- dual_port_ram.v
+++ dual_port_ram.v
@@ -1,1 +1,19 @@
+module dual_port_ram (
+    input clk, we_a, we_b,
+    input [3:0] addr_a, addr_b,
+    input [7:0] din_a, din_b, 
+    output reg [7:0] dout_a, dout_b
+);
+    reg [7:0] ram [15:0]; 
 
+    always @(posedge clk)
+    begin
+        if (we_a)
+            ram[addr_a] <= din_a;
+        dout_a <= ram[addr_a];
+
+        if (we_b)
+            ram[addr_b] <= din_b;
+        dout_b <= ram[addr_b];
+    end
+endmodule
